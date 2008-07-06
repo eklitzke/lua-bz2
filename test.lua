bd60259a5df92e8b8828ad953ec3b317c8f2afc2
Evan Klitzke <evan@yelp.com>
2008-07-05 18:43:56 -0700

    make unit test more elegant lua (i think)

--- test.lua
+++ test.lua
@@ -1,8 +1,12 @@
 require("bz2")
 
+-- read in a file and display the uncompressed data to stdout (i.e. this
+-- behaves identically to bzcat)
 b = bz2.read_open("access_log.bz2")
-while 1 do
-	text = bz2.read(b, 10)
+text = bz2.read(b, 1024)
+while text ~= nil do
+	io.write(text)
+	text = bz2.read(b, 1024)
 	if text == nil then
 		break
 	end
